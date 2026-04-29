0c7113f39f2249b24827a33895752fcaa00b34aa
lyonmu <lyonmu@foxmail.com>
2026-04-29 17:35:45 +0800

    style: fix formatting in logger test file

--- pkg/logger/logger_test.go
+++ pkg/logger/logger_test.go
@@ -232,27 +232,27 @@ type testArrayEncoder struct {
 	strings []string
 }
 
-func (e *testArrayEncoder) AppendBool(bool)         {}
-func (e *testArrayEncoder) AppendByte(byte)         {}
-func (e *testArrayEncoder) AppendComplex128(complex128) {}
-func (e *testArrayEncoder) AppendComplex64(complex64)   {}
-func (e *testArrayEncoder) AppendDuration(time.Duration) {}
-func (e *testArrayEncoder) AppendFloat64(float64)   {}
-func (e *testArrayEncoder) AppendFloat32(float32)   {}
-func (e *testArrayEncoder) AppendInt(int)           {}
-func (e *testArrayEncoder) AppendInt64(int64)       {}
-func (e *testArrayEncoder) AppendInt32(int32)       {}
-func (e *testArrayEncoder) AppendInt16(int16)       {}
-func (e *testArrayEncoder) AppendInt8(int8)         {}
-func (e *testArrayEncoder) AppendString(s string)   { e.strings = append(e.strings, s) }
-func (e *testArrayEncoder) AppendTime(time.Time)    {}
-func (e *testArrayEncoder) AppendUint(uint)         {}
-func (e *testArrayEncoder) AppendUint64(uint64)     {}
-func (e *testArrayEncoder) AppendUint32(uint32)     {}
-func (e *testArrayEncoder) AppendUint16(uint16)     {}
-func (e *testArrayEncoder) AppendUint8(uint8)       {}
-func (e *testArrayEncoder) AppendUintptr(uintptr)   {}
+func (e *testArrayEncoder) AppendBool(bool)                            {}
+func (e *testArrayEncoder) AppendByte(byte)                            {}
+func (e *testArrayEncoder) AppendComplex128(complex128)                {}
+func (e *testArrayEncoder) AppendComplex64(complex64)                  {}
+func (e *testArrayEncoder) AppendDuration(time.Duration)               {}
+func (e *testArrayEncoder) AppendFloat64(float64)                      {}
+func (e *testArrayEncoder) AppendFloat32(float32)                      {}
+func (e *testArrayEncoder) AppendInt(int)                              {}
+func (e *testArrayEncoder) AppendInt64(int64)                          {}
+func (e *testArrayEncoder) AppendInt32(int32)                          {}
+func (e *testArrayEncoder) AppendInt16(int16)                          {}
+func (e *testArrayEncoder) AppendInt8(int8)                            {}
+func (e *testArrayEncoder) AppendString(s string)                      { e.strings = append(e.strings, s) }
+func (e *testArrayEncoder) AppendTime(time.Time)                       {}
+func (e *testArrayEncoder) AppendUint(uint)                            {}
+func (e *testArrayEncoder) AppendUint64(uint64)                        {}
+func (e *testArrayEncoder) AppendUint32(uint32)                        {}
+func (e *testArrayEncoder) AppendUint16(uint16)                        {}
+func (e *testArrayEncoder) AppendUint8(uint8)                          {}
+func (e *testArrayEncoder) AppendUintptr(uintptr)                      {}
 func (e *testArrayEncoder) AppendArray(zapcore.ArrayMarshaler) error   { return nil }
 func (e *testArrayEncoder) AppendObject(zapcore.ObjectMarshaler) error { return nil }
 func (e *testArrayEncoder) AppendReflected(interface{}) error          { return nil }
-func (e *testArrayEncoder) AppendByteString([]byte)                   {}
+func (e *testArrayEncoder) AppendByteString([]byte)                    {}
